87a8e6ceba116c89d3a5604b8602e925e558fc01
Germano06 <57481143+Germano06@users.noreply.github.com>
2022-06-15 01:15:03 +0530

    completed

--- nQueen/nqueen.py
+++ nQueen/nqueen.py
@@ -1,14 +1,14 @@
-def placeQueen(q, r, c, brd):
+def placeQueen(r, c, brd):
     for y in range(c):
-        if brd[r][y] == 1:
+        if brd[r][y] != '+':
             return False
 
     for i, j in zip(range(r, -1, -1), range(c, -1, -1)):
-        if brd[i][j] == 1:
+        if brd[i][j] != '+':
             return False
 
     for i, j in zip(range(r, n, 1), range(c, -1, -1)):
-        if brd[i][j] == 1:
+        if brd[i][j] != '+':
             return False
     return True
 
@@ -24,7 +24,7 @@ def showBoard(b):
     print()
     for x in range(n):
         for y in range(n):
-            print(b[x][y], end=" ")
+            print(b[x][y], end="  ")
         print()
 
 
@@ -33,26 +33,23 @@ def tryQueen(c, brd):
         return True
     for row in range(n):
         x = "Q"+str(c+1)
-        if placeQueen(x, row, c, brd):
+        if placeQueen(row, c, brd):
             queens[x] = [row, c]
-            brd[row][c] = 1
-            if tryQueen(c+1, brd):
-                if placed():
-                    showBoard(brd)
-                    queens[x] = [-1, -1]
-                    if placeQueen(x, row, c, brd):
-                        brd[row][c] = 0
-                return True
-            brd[row][c] = 0
+            brd[row][c] = row+1
+            tryQueen(c+1, brd)
+            if placed():
+                showBoard(brd)
+                queens[x] = [-1, -1]
+                if placeQueen(row, c, brd):
+                    brd[row][c] = '+'
+            brd[row][c] = '+'
     return False
 
 
 n = int(input("Enter the number of queens: "))
-cbrd = [[0 for i in range(n)] for j in range(n)]
+cbrd = [['+' for i in range(n)] for j in range(n)]
 queens = {}
 for i in range(n):
     x = "Q"+str(i+1)
     queens[x] = [-1, -1]
-flag = False
-if not tryQueen(0, cbrd):
-    print("Solution not found\n")
+tryQueen(0, cbrd)
